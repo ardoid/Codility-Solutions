75075598cd35a3473efb1414b5703b285c51789a
ardoid <ardoid@users.noreply.github.com>
2015-07-17 00:11:35 +0200

    Update NumberSolitaire.java

--- Dynamic-programming/NumberSolitaire.java
+++ Dynamic-programming/NumberSolitaire.java
@@ -1,9 +1,10 @@
 class Solution {
     public int solution(int[] A) {
+        //int[] sum = new int[A.length+1];
         final int MIN = -10001;
         int ans = A[0];
         int skip = 0;
-        int maxskip = MIN;
+        int maxskip = MIN, nexskip = MIN;
         int idxskip = 0;
         for (int i=1; i<A.length-1; ++i)  {
             if (A[i] >= 0) {
@@ -14,12 +15,16 @@ class Solution {
                 skip++;
                 if (A[i] >= maxskip)  {
                     maxskip = A[i];
+                    nexskip = MIN;
                     idxskip = skip;
+                } else {
+                    nexskip = Math.max(A[i], nexskip);
                 }
                 if (skip >= 6)  {
                     ans += maxskip;
                     skip = 6 - idxskip;
-                    maxskip = MIN;
+                    maxskip = nexskip;
+                    nexskip = MIN;
                 }
             }
         }
